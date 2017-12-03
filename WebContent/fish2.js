7a5b7efee0891e7d93f0ee7258cb009bddbc51e5
ameza <adrianameza@dhcp-10-8-055-252.mobile.reshsg.uci.edu>
2017-12-02 19:19:14 -0800

    v3
    -on mouse over for links implemented

--- WebContent/fish2.js
+++ WebContent/fish2.js
@@ -1,6 +1,30 @@
 var w = 1600,
     h = 800,
-    fill = d3.scale.category20() //provides a new instance of the sacle
+    /**
+     * d3.scale: provides a new instance of the scale
+     * category20: is a scale of colors
+     *  0   #1f77b4
+		1   #aec7e8
+		2   #ff7f0e
+		3   #ffbb78
+		4   #2ca02c
+		5   #98df8a
+		6   #d62728
+		7   #ff9896
+		8   #9467bd
+		9   #c5b0d5
+		10  #8c564b
+		11  #c49c94
+		12  #e377c2
+		13  #f7b6d2
+		14  #7f7f7f
+		15  #c7c7c7
+		16  #bcbd22
+		17  #dbdb8d
+		18  #17becf
+		19  #9edae5
+     */
+    fill = d3.scale.category20() 
     trans=[0,0]
     scale=1;
 
@@ -8,31 +32,48 @@ var fisheye = d3.fisheye()
     .radius(100)
     .power(3);
 
-/*
- * g: is a container used to group other SVG elements.
+/* g: is a container used to group other SVG elements.
  * Transformations applied to the <g> element are performed on all of its child elements.*/
-var vis = d3.select("#chart") //chart is a html div object
-  .append("svg:svg") //agrega un svg a chart
-    //chart size
-    .attr("width", w)
+
+/* d3.selcect: select an element in the page, in this case the <div> con id=chart */
+var vis = d3.select("#chart") 
+  .append("svg:svg") //agrega un svg a div element called chart
+    .attr("width", w) 
     .attr("height", h)
     .attr("pointer-events", "all")
   .append('svg:g')
     .call(d3.behavior.zoom().on("zoom", redraw))
   .append('svg:g');
 
+//Notation: the notation above is the same that:
+/*
+ * var vis = d3.select("#chart");
+ * append("svg:svg");
+ * attr("width", w);
+ * etc...
+ * 
+ * If a value is passed as parameter, it is a setter function
+ * If none value is passed as parameter, it is a getter function
+ * */
+
 vis.append('svg:rect')
     .attr('width', w)
     .attr('height', h)
-    .attr('fill', 'white'); //assign scale instance (fill object) a color
+    .attr('fill', 'white'); //fill is a scale of colors
 
+//Function called when zoom change
 function redraw() {
   trans=d3.event.translate; 
   scale=d3.event.scale;
 
   vis.attr("transform",
       "translate(" + trans + ")"
-      + " scale(" + scale + ")"); //transform move all the points
+      + " scale(" + scale + ")"); 
+  
+  /*TEST*/
+  console.log("redraw=" + "transform",
+      "translate(" + trans + ")"
+      + " scale(" + scale + ")"); 
 }
 
 var draw = function(json) {
@@ -45,36 +86,35 @@ var draw = function(json) {
       .size([w, h]);
       //.start();
 
-  //write the links
+  //@ameza: to configure links
   var link = vis.selectAll("line.link") //CSS class
       .data(json.links)
-    .enter().append("svg:line") //Add line to the svg container
-      .attr("class", "link") //To apply CSS class to link object
-      .style("stroke-width", function(d) { return Math.sqrt(d.value); })
-      //Assign data to the line
+    .enter().append("svg:line") //Add link to the svg container
+      .attr("class", "link") //CSS class
+      //.style("stroke-width", function(d) { return Math.sqrt(d.value); }) //OLD
+      //.style("stroke-width", 1)
+      //Declare link's attributes: source and target coordinates
       .attr("x1", function(d) { return d.source.x; }) 
       .attr("y1", function(d) { return d.source.y; })
       .attr("x2", function(d) { return d.target.x; })
       .attr("y2", function(d) { return d.target.y; })
-     //Assign events to the line
+      //Assign events to the link
     .on("click", clickLink)
-    //.on ("mouseover",moverLink) //TO DO: Not working right 
-    //.on ("mouseout",moutLink)
+    .on ("mouseover",moverLink) //TO DO: Not working right 
+    .on ("mouseout",moutLink)
     ;
 
-  //write the nodes
+  //@ameza: to configure nodes
   var node = vis.selectAll("circle.node") //CSS class
       .data(json.nodes)
     .enter().append("svg:circle") //Add node to the svg container
       .attr("class", "node") //CSS class
-      //Assign data to the node
-      .attr("cx", function(d) { return d.x; })
+      //Declare node's attributes 
+      .attr("cx", function(d) { return d.x; }) //center position 
       .attr("cy", function(d) { return d.y; })
-      .attr("r", 5)
-      .attr("component", function(d) { return d.component;}) //TEST
-      //.style("fill", function(d) { return fill(d.group); }) //OLD
-      //@ameza: Assign the color to each node based on the app name
-      .style("fill", function(d) { return fill(d.apppackage); }) 
+      .attr("r", 5) //radious length
+      //.attr("component", function(d) { return d.component;}) //To delete
+      .style("fill", function(d) { return fill(d.apppackage); }) //Assign the color to each node based on the app name
     //Assign events to the node
 	.on("click", clickNode)
     .on ("mouseover",moverNode)
@@ -94,25 +134,30 @@ var draw = function(json) {
     for (var i = n; i > 0; --i) force.tick();
     force.stop();
 
-    //on click event for link
+    //@ameza:on click event for link
 	function clickLink(d) {
 		console.log("Communication/Permission: "+d.type); //TO DO: Display in a specific area in the layout
 	}
-    //on click event for nodes
+    //@ameza:on click event for nodes
 	function clickNode(d) {
-		console.log("click on node"); //TO DO: Method to get the component info from the app XML file
+		console.log("click on node: "+d.id); //TO DO: Method to get the component info from the app XML file
 	}
-    //mouse over event: it paints the pop-up over the nodes, manipulating the CSS
+	
+	//@ameza: on mouse over event this method paints the pop-up next to the node manipulating the CSS of the div object
     function moverNode(d) {
         $("#pop-up").fadeOut(100,function () {
-            // Popup content
-            //$("#pop-up-title").html("M: text "); //OLD
-        	    $("#pop-up-title").html("Package: "+d.apppackage); //@ameza: displaying the node's package name
-        	    $("#pop-up-subtitle").html("Component: "+d.component); //@ameza: displaying the node's component name
+        	    $("#pop-up-title").html("Package: "+d.apppackage); 
+        	    $("#pop-up-subtitle").html("Component: "+d.component); 
             /*$("#pop-img").html("23");
-            $("#pop-desc").html("M+T: text text test"); *///TO DO: Method to get the component info from the app XML file
+            $("#pop-desc").html("M+T: text text test"); */ //TO DO: Method to get the component info from the app XML file
 
             // Popup position
+        	    console.log(scale);
+        	    console.log("trans[0]="+trans[0]);
+        	    console.log("d.x="+d.x);
+        	    console.log("trans[1]="+trans[1]);
+        	    console.log("d.y="+d.y);
+        	    
             var popLeft = (d.x*scale)+trans[0]+20;//lE.cL[0] + 20;
             var popTop = (d.y*scale)+trans[1]+20;//lE.cL[1] + 70;
             $("#pop-up").css({"left":popLeft,"top":popTop});
@@ -120,40 +165,42 @@ var draw = function(json) {
         });
 
     }
-    //mouse over event: it paints the pop-up over the nodes, manipulating the CSS
+    //@ameza: on mouse over event this method paints the pop-up next to the link manipulating the CSS of the div object
     function moverLink(d) {
         $("#pop-up-link").fadeOut(100,function () {
-            // Popup content
-            //$("#pop-up-title").html("M: text "); //OLD
-        	    $("#pop-up-link-type").html("Communication/Permission: "+d.type); //@ameza: displaying the node's package name
-            // Popup position
-            var popLeft = (d.source.x*scale)+trans[0]+20;//lE.cL[0] + 20; //TO DO: Not working right 
-            var popTop = (d.source.y*scale)+trans[1]+20;//lE.cL[1] + 70;
+        	    var cType = "Permission: ";
+        	    if ((d.type =="implicit") || (d.type=="explicit")) {
+        	    		cType = "Communication: ";
+        	    }
+        	    
+        	    $("#pop-up-link-type").html(cType+d.type); 
+            var popLeft = (d.source.x); //TO DO: Improve to display pop-up next to the line
+            var popTop = (d.source.y);
             $("#pop-up-link").css({"left":popLeft,"top":popTop});
             $("#pop-up-link").fadeIn(100);
         });
 
     }
-    //mouseout
+    //@ameza: on mouseout the pop-up disappears
     function moutNode(d) {
         $("#pop-up").fadeOut(50);
         d3.select(this).attr("fill","url(#ten1)");
     }
-    //mouseout
+    //@ameza: on mouseout the pop-up disappears
     function moutLink(d) {
         $("#pop-up-link").fadeOut(50);
         d3.select(this).attr("fill","url(#ten1)"); 
     }
-   //This on.mousemove event is for the vis object. It applies the fisheye effect
+   //This on mousemove event is for the vis object. It applies the fisheye effect
    vis.on("mousemove", function() {
-       // d is the point over which the mouse is
+       
        fisheye.center(d3.mouse(this));
-
+       //To get data from the node us "d"
        node
            .each(function(d) { d.display = fisheye(d); })
            .attr("cx", function(d) { return d.display.x; }) //center x coordinate does not change
            .attr("cy", function(d) { return d.display.y; }) //center y coordinate does not change
-           .attr("r", function(d) { return d.display.z * 4.5; }); //circle radious doues change, thus the point size change
+           .attr("r", function(d) { return d.display.z * 4.5; }); //circle radious does change, thus the point size change
 
        link
            .attr("x1", function(d) { return d.source.display.x; }) //start point x coordinate does not change
